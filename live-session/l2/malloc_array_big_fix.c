543f058cd4d19c381c074d3d4a79f0e40d973e5d
Anna Arpaci-Dusseau <anna.arpacidusseau@gmail.com>
2026-09-03 15:14:50 -0400

    l2 code

---
+++ live-session/l2/malloc_array_big_fix.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int print_numbers(void) {
+  int* arr = malloc(sizeof(int)*100000);
+
+  if(arr == NULL) {
+    return 1;
+  }
+
+  for(int i = 0; i < 30; i++) {
+    arr[i] = i*3;
+  }
+
+  for(int i = 0; i < 30; i++) {
+    printf("%d ", *(arr + i));
+  }
+  printf("\n");
+  free(arr);
+  return 0;
+}
+
+int main(void) {
+  while(1) {
+    print_numbers();
+  }
+}
---
+++ live-session/l2/numbers.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int print_numbers(void) {
+  int* arr = malloc(sizeof(int)*100000);
+
+  if(arr == NULL) {
+    printf("Malloc FAILED\n");
+    exit(1);
+  }
+
+  for(int i = 0; i < 30; i++) {
+    arr[i] = i*3;
+  }
+
+  for(int i = 0; i < 30; i++) {
+    printf("%d ", *(arr + i));
+  }
+  printf("\n");
+  return 0;
+}
+
+int main(void) {
+  print_numbers();
+}
